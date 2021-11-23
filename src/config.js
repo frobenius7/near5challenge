42e1948ef4f849fa33f95ebe3e310959a4e2a883
frobenius7 <vetb@me.com>
2021-11-23 14:00:46 +0300

    again blyats 3

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME ||'vetal.testnet'
+const CONTRACT_NAME = process.env.CONTRACT_NAME ||'newnft.vetal.testnet'
 
 function getConfig(env) {
   switch (env) {
