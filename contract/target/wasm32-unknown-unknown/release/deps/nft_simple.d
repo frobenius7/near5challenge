d011df47131e04a6ddf86ed720d2853f7688e3e3
frobenius7 <vetb@me.com>
2021-11-23 12:55:00 +0300

    some tweaks

--- contract/target/wasm32-unknown-unknown/release/deps/nft_simple.d
+++ contract/target/wasm32-unknown-unknown/release/deps/nft_simple.d
@@ -1,8 +1,8 @@
-/Users/vadkutsen/rust/near/near-challenge-5/contract/target/wasm32-unknown-unknown/release/deps/nft_simple.wasm: src/lib.rs src/internal.rs src/approval.rs src/enumeration.rs src/metadata.rs src/mint.rs src/nft_core.rs src/royalty.rs
+/Users/vb/dev/near5challenge/contract/target/wasm32-unknown-unknown/release/deps/nft_simple.wasm: src/lib.rs src/internal.rs src/approval.rs src/enumeration.rs src/metadata.rs src/mint.rs src/nft_core.rs src/royalty.rs
 
-/Users/vadkutsen/rust/near/near-challenge-5/contract/target/wasm32-unknown-unknown/release/deps/libnft_simple.rlib: src/lib.rs src/internal.rs src/approval.rs src/enumeration.rs src/metadata.rs src/mint.rs src/nft_core.rs src/royalty.rs
+/Users/vb/dev/near5challenge/contract/target/wasm32-unknown-unknown/release/deps/libnft_simple.rlib: src/lib.rs src/internal.rs src/approval.rs src/enumeration.rs src/metadata.rs src/mint.rs src/nft_core.rs src/royalty.rs
 
-/Users/vadkutsen/rust/near/near-challenge-5/contract/target/wasm32-unknown-unknown/release/deps/nft_simple.d: src/lib.rs src/internal.rs src/approval.rs src/enumeration.rs src/metadata.rs src/mint.rs src/nft_core.rs src/royalty.rs
+/Users/vb/dev/near5challenge/contract/target/wasm32-unknown-unknown/release/deps/nft_simple.d: src/lib.rs src/internal.rs src/approval.rs src/enumeration.rs src/metadata.rs src/mint.rs src/nft_core.rs src/royalty.rs
 
 src/lib.rs:
 src/internal.rs:
